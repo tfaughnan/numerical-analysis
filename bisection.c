edf7af4a1395fd0a66b7570c07a014d83d703f91
tfaughnan <tfaughnan13@gmail.com>
2020-09-15 14:44:26 -0400

    add bisection method

---
+++ bisection.c
@@ -0,0 +1,49 @@
+// bisection method
+
+#include <stdio.h>
+#include <math.h>
+
+double f(double x);
+
+int main()
+{
+    // initial bounds
+    // requires a < b, f(a) * f(b) < 0
+    double a = 0;
+    double b = 2;
+
+    // error tolerance
+    double TOL = 10e-5;
+
+    // maximum iterations
+    int N = 100;
+
+    printf("n\tp\n");
+    double p;
+    int n = 1;
+    while (n <= N)
+    {
+        p = (a + b) / 2;
+        printf("%d\t%lf\n", n, p);
+
+        // stopping criterion
+        // alternatives exist, e.g. |f(p)| <= TOL
+        if ((b - a) / 2 <= TOL)
+            break;
+
+        if (f(a) * f(p) > 0)
+            a = p;
+        else
+            b = p;
+        n++;
+    }
+
+    return 0;
+}
+
+double f(double x)
+{
+    // example function f(x)=x^2-x-1
+    return pow(x, 2) - x - 1;
+}
+
